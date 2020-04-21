5a64248df723a96e397ab6acba8e70c71298cd1b
AndLvG <realandmir@gmail.com>
2020-04-21 12:52:54 +0300

    first

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    app.run()
+
+# pip install flask
+# pip install flask-ngrok
+# pip freeze > requirements.txt
